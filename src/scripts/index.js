67e1a8994a26a23df6437da8dabe66376803e1d3
CarloA <carlosab3007@gmail.com>
2025-04-03 20:31:37 -0500

    finish the adding of modal

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,38 @@
-import '../styles.css'
+import "../styles.css";
+
+class TodoCard {
+  constructor(title, description, priority) {
+    this.title = title;
+    this.description = description;
+    this.priority = priority;
+  }
+}
+
+class todoList {
+  constructor() {
+    this.cards = [];
+  }
+  addCard(title, description, priority) {
+    const newCard = new TodoCard(title, description, priority);
+    this.cards.push(newCard);
+  }
+}
+
+class modalController {
+  constructor(TodoCard) {
+    (this.todocard = TodoCard),
+      (this.container = document.getElementById("container"));
+    this.addTodo = document.getElementById("addTodo");
+    this.modal = document.getElementById("modal");
+    this.closeModal = document.getElementById("close");
+    this.submitModal = document.getElementById("sumbit");
+    this.setUpListeners();
+  }
+
+  setUpListeners() {
+    this.addTodo.addEventListener("click", () => this.modal.classList.add("show"));
+    this.closeModal.addEventListener("click", () => this.modal.classList.remove("show"));
+
+  }
+}
+const modalcontroller = new modalController(1);
